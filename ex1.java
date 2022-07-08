1da6028e351693ab4384db243e6e42186e57f6a6
David <dehoustdavid@gmail.com>
2022-07-08 15:44:29 +0200

    Ajout du fichier .gitignore

--- ex1.java
+++ ex1.java
@@ -5,26 +5,28 @@ public class ex1 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Quel est ton prénom ? ");
         String prenom = input.nextLine();
 
         System.out.print("Quel est ton nom ? ");
-        String nom = input.nextLine();        
+        String nom = input.nextLine();
 
         input.close();
 
         DisplayFullName(prenom, nom);
+
     }
-    static void DisplayFullName(String _prenom, String _nom){
-        if(_prenom.equals("David") && _nom.equals("Dehoust")){
+
+    static void DisplayFullName(String _prenom, String _nom) {
+        if (_prenom.equals("David") && _nom.equals("Dehoust")) {
             System.out.println("Bonjour admin");
-        } else{
+        } else {
             System.out.println(_prenom + " " + _nom);
         }
     }
 
-    static void DisplayFirstName(String _prenom){
+    static void DisplayFirstName(String _prenom) {
         System.out.println(_prenom);
     }
 }
