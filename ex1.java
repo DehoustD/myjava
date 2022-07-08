1dcc4e55568fa6b2270fcdd89f714360c62ddb2d
David <dehoustdavid@gmail.com>
2022-07-08 11:48:02 +0200

    yo

---
+++ ex1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Quel est ton prénom ?");
+        String prenom = input.nextLine();
+
+        System.out.println("Quel est ton nom ?");
+        String nom = input.nextLine();
+
+        System.out.println(prenom +);
+
+        input.close();
+        // salut
+    
+    }
+}
